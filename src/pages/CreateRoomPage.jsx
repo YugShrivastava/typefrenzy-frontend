c5426f651af6cc93dbf16ace6773b0b84d6a5e98
YugShrivastava <shrivastavayug04@gmail.com>
2026-03-23 22:19:18 +0530

    setup next

--- src/pages/CreateRoomPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function CreateRoomPage() {
-  return (
-    <div>CreateRoomPage</div>
-  )
-}
-
-export default CreateRoomPage
--- src/pages/LeaderBoardPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function LeaderBoardPage() {
-  return (
-    <div>LeaderBoardPage</div>
-  )
-}
-
-export default LeaderBoardPage
